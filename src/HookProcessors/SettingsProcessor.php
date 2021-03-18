3fa3a87ff0c0115225122f2c52756dcfcbf84c17
yurii <yurii@wordproof.com>
2021-03-18 18:19:13 +0200

    Added authorizeRedirect method

--- src/HookProcessors/SettingsProcessor.php
+++ src/HookProcessors/SettingsProcessor.php
@@ -6,13 +6,33 @@ namespace WordProof\Wordpress\HookProcessors;
 
 class SettingsProcessor implements Processor
 {
+    private array $settings = [
+        'endpoint' => 'http://my.wordproof.com/',
+        'scope' => ''
+    ];
+    
     public function __construct(array $settings)
     {
+        $this->setSettings($settings);
+        
         add_action('plugins_loaded', [$this, 'init']);
     }
     
     public function init()
     {
         //
+    }
+    
+    public function getSetting($key)
+    {
+        return isset($this->settings[$key]) ? $this->settings[$key] : null;
+    }
+    
+    public function setSettings($key, $value = null)
+    {
+        if (is_array($key))
+            $this->settings = array_merge($this->settings, $key);
+        
+        $this->settings[$key] = $value;
     }
 }
--- src/WordProofTimestamp.php
+++ src/WordProofTimestamp.php
@@ -10,15 +10,23 @@ use WordProof\Wordpress\HookProcessors\SettingsProcessor;
 
 class WordProofTimestamp
 {
+    private int $clientId;
+    
+    private string $clientSecret;
+    
     private ?BulkProcessor $bulkProcessor = null;
     
     private ?MetaBoxesProcessor $metaBoxesProcessor = null;
     
     private ?SettingsProcessor $settingsProcessor = null;
     
-    public function __construct()
+    private Vendor\WordProof\ApiClient\WordProofApi $client;
+    
+    public function __construct(int $clientId, string $clientSecret)
     {
-        //
+        $this->clientId = $clientId;
+        $this->clientSecret = str_replace('"','', str_replace("'","", $clientSecret));
+        $this->client = new Vendor\WordProof\ApiClient\WordProofApi();
     }
     
     public function withMetaBoxes(): self
@@ -37,5 +45,19 @@ class WordProofTimestamp
     {
         $this->settingsProcessor = new SettingsProcessor($settings);
         return $this;
+    }
+    
+    public function authorizeRedirect()
+    {
+        $params = [
+            'client_id' => $this->clientId,
+            'redirect_uri' => $this->settingsProcessor->getSetting('redirect_uri'),
+            'response_type' => $this->settingsProcessor->getSetting('response_type'),
+            'scope' => $this->settingsProcessor->getSetting('scope'),
+        ];
+        
+        $url = $this->settingsProcessor->getSetting('endpoint') . "/oauth/authorize?" . http_build_query($params);
+        
+        header("Location: " . $url);
     }
 }
