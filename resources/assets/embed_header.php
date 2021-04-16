bc5803121dbf4f2e169fd566e2ec0c96bf737839
yurii <yurii@wordproof.com>
2021-04-16 13:09:05 +0300

    remove unused templates

--- resources/assets/embed_header.php
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" href="<?php echo plugin_dir_url(__FILE__); ?>/styles/wordpress-sdk.css">
-
-<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
-
-<script type="text/javascript">
-    window.WordproofSDK = {
-        endpoint: function () { return "<?php echo $endpoint ?? ""; ?>" },
-    };
-</script>
