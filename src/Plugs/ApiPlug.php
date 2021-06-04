cc44faae42498b0a8cf59e0f479a8419cc96ca7c
Marijn Bent <marijn@wordproof.com>
2021-06-04 10:01:49 +0200

    add hashinput endpoint

--- src/Plugs/ApiPlug.php
+++ src/Plugs/ApiPlug.php
@@ -2,6 +2,7 @@
 
 namespace WordProof\SDK\Plugs;
 
+use WordProof\SDK\Helpers\PostMeta;
 use WordProof\SDK\Support\Authentication;
 
 class ApiPlug
@@ -18,17 +19,36 @@ class ApiPlug
             'methods'  => 'POST',
             'callback' => [$this, 'webhook'],
         ]);
+        
+        register_rest_route('wordproof/v1', '/posts/(?P<id>\d+)/hashinput', [
+            'methods'  => 'GET',
+            'callback' => [$this, 'hashInput'],
+        ]);
+    }
+    
+    public function hashInput(\WP_REST_Request $request)
+    {
+        $data = $request->get_params();
+        $postId = intval($data['id']);
+        
+        return get_post_meta($postId, 'wordproof_hash_input', true);
     }
     
     public function oauthCallback()
     {
         Authentication::token();
-        //TODO what now?
     }
     
     public function webhook(\WP_REST_Request $request)
     {
-        ray($request);
+        if (Authentication::isValidRequest($request)) {
+    
+            $response = json_decode($request->get_body());
+            $postId = intval($response->uid);
+            $schema = $response->schema;
+
+            PostMeta::set($postId, 'wordproof_schema', $schema);
+        }
     }
 
 }
