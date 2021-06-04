ef99be812943c335b3254fa2e44ec191bbd57a02
Marijn Bent <marijn@wordproof.com>
2021-06-04 10:01:56 +0200

    add new plug

--- src/WordProofTimestamp.php
+++ src/WordProofTimestamp.php
@@ -6,9 +6,9 @@ use Throwable;
 use WordProof\SDK\Plugs\ApiPlug;
 use WordProof\SDK\Plugs\AuthenticationPlug;
 use WordProof\SDK\Plugs\CertificatePlug;
+use WordProof\SDK\Plugs\TimestampPlug;
 use WordProof\SDK\Plugs\WebhookPlug;
 use WordProof\SDK\Support\Loader;
-use WordProof\SDK\Vendor\WordProof\ApiClient\WordProofApi;
 
 class WordProofTimestamp
 {
@@ -26,45 +26,60 @@ class WordProofTimestamp
     {
         $this->loader = new Loader();
     
+        if (!headers_sent() && !session_id())
+            session_start();
+        
         $this->constants();
         $this->authentication();
         $this->api();
-        $this->webhook();
+//        $this->webhook();
         $this->certificate();
-
+        $this->timestamp();
+        
         $this->loader->run();
     }
     
-    public function constants() {
+    public function constants()
+    {
         define('WORDPROOF_URL', 'https://myv2.test');
         define('WORDPROOF_CLIENT', 3);
     }
     
-    public function authentication() {
+    public function authentication()
+    {
         $class = new AuthenticationPlug();
-
+        
         $this->loader->add_action('wordproof_authenticate', $class, 'authenticate');
     }
     
-    public function api() {
+    public function api()
+    {
         $class = new ApiPlug();
         
         $this->loader->add_action('rest_api_init', $class, 'init');
     }
     
-    public function webhook() {
+    public function webhook()
+    {
         $class = new WebhookPlug();
         
-        $this->loader->add_action('wordproof_webhook', $class, 'webhook');
+//        $this->loader->add_action('wordproof_webhook', $class, 'webhook');
     }
     
-    
-    public function certificate() {
+    public function certificate()
+    {
         $class = new CertificatePlug();
-    
+        
         $this->loader->add_action('wp_head', $class, 'head');
         $this->loader->add_filter('the_content', $class, 'certificateTag');
     }
-
-
+    
+    public function timestamp()
+    {
+        $class = new TimestampPlug();
+        
+        $this->loader->add_action('wordproof_timestamp', $class, 'timestamp');
+    }
+    
+    
 }
