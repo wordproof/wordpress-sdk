8c3f4a7bebb7d9efa49598bf2cd09ce0361b1999
Marijn Bent <marijn@wordproof.com>
2022-01-02 14:51:15 +0700

    localize post editor js

---
+++ src/Controllers/PostEditorController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace WordProof\SDK\Controllers;
+
+use WordProof\SDK\Helpers\PostEditor;
+use WordProof\SDK\Helpers\Settings;
+use WordProof\SDK\Support\Authentication;
+
+class PostEditorController
+{
+    
+    public function __construct() {}
+    
+    public function localizeScripts($hook)
+    {
+        if ( ! PostEditor::isPostEdit($hook) )
+            return;
+        
+        //make retrievable via ajax
+        
+        $currentPostType = PostEditor::getCurrentPostType();
+        $data = [
+            'data' => [
+                'is_authenticated'                  => Authentication::isAuthenticated(),
+                'popup_redirect_authentication_url' => admin_url('admin.php?page=wordproof-redirect-authenticate'),
+                'popup_redirect_settings_url'       => admin_url('admin.php?page=wordproof-redirect-settings'),
+                'settings'                          => Settings::get(),
+                'timestamp_current_post_type'       => Settings::postTypeIsInSelectedPostTypes($currentPostType),
+                'current_post_type'       => $currentPostType,
+            ],
+        ];
+        
+        //TODO Register own script
+        wp_localize_script('yoast-seo-post-edit', 'wordproofSdk', $data);
+        wp_localize_script('yoast-seo-post-edit-classic', 'wordproofSdk', $data);
+    }
+}
