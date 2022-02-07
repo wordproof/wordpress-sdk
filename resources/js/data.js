b894d273e415547ad0c62aad719387bf387e682d
Marijn Bent <marijn@wordproof.com>
2022-02-07 14:27:44 +0700

    add simple store

---
+++ resources/js/data.js
@@ -0,0 +1,72 @@
+const {createReduxStore, registerStore, register} = wp.data;
+
+const storeName = 'wordproof';
+const initialState = {
+    isAuthenticated: true,
+};
+
+const actions = {
+    setIsAuthenticated(isAuthenticated) {
+        return {
+            type: 'SET_IS_AUTHENTICATED',
+            isAuthenticated,
+        };
+    },
+    getIsAuthenticated() {
+        return {
+            type: 'GET_IS_AUTHENTICATED',
+        };
+    },
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_IS_AUTHENTICATED': {
+            return {
+                ...state,
+                isAuthenticated: action.isAuthenticated,
+            };
+        }
+        default: {
+            return state;
+        }
+    }
+};
+
+const selectors = {
+    getIsAuthenticated(state) {
+        const {isAuthenticated} = state;
+        return isAuthenticated;
+    },
+};
+
+const controls = {
+    fetchIsAuthenticated(action) {
+        return apiFetch({path: action.path});
+    },
+};
+const resolvers = {
+    * fetchIsAuthenticated() {
+        const isAuthenticated = yield actions.getIsAuthenticated();
+        return actions.setIsAuthenticated(isAuthenticated);
+    },
+};
+
+const storeConfig = {
+    reducer,
+    controls,
+    selectors,
+    resolvers,
+    actions,
+};
+
+if (createReduxStore) {
+    const store = createReduxStore("wordproof", storeConfig);
+    register(store);
+} else {
+    /*
+    * Compatibility fix for WP 5.6.
+    * Remove this and the related import when WP 5.6 is no longer supported.
+    */
+    registerStore("wordproof", storeConfig);
+}
