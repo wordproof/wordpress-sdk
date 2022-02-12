1a1b2b9f38f8a75479a3dec15e86b1db46386222
Marijn Bent <marijn@wordproof.com>
2022-02-12 11:33:19 +0700

    Add components to display modals

---
+++ resources/js/components/AuthenticationResultModals.js
@@ -0,0 +1,48 @@
+const {useState} = wp.element;
+
+import OAuthDeniedModal from "./modals/OAuthDeniedModal";
+import WebhookSuccessModal from "./modals/WebhookSuccessModal";
+import WebhookFailedModal from "./modals/WebhookFailedModal";
+
+const AuthenticationResultModals = () => {
+
+    const {modal, setModal} = useState(null);
+
+    window.addEventListener('wordproof:oauth:success', () => {
+        setModal('oauth:success');
+    }, false);
+
+    window.addEventListener('wordproof:oauth:failed', () => {
+        setModal('oauth:failed');
+    }, false);
+
+    window.addEventListener('wordproof:oauth:denied', () => {
+        setModal('oauth:denied');
+    }, false);
+
+    window.addEventListener('wordproof:webhook:failed', () => {
+        setModal('webhook:failed');
+    }, false);
+
+    return (
+            <>
+                {modal === 'oauth:success' &&
+                <WebhookSuccessModal/>
+                }
+
+                {modal === 'oauth:denied' &&
+                <OAuthDeniedModal/>
+                }
+
+                {modal === 'oauth:failed' &&
+                <OAuthDeniedModal/>
+                }
+
+                {modal === 'webhook:failed' &&
+                <WebhookFailedModal/>
+                }
+            </>
+    );
+}
+
+export default AuthenticationResultModals;
