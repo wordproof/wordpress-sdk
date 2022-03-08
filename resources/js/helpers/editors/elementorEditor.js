1d1599b941939a06c225d383796e54a10f52dde4
Marijn Bent <marijn@wordproof.com>
2022-03-08 12:35:01 +0700

    lint js

--- resources/js/helpers/editors/elementorEditor.js
+++ resources/js/helpers/editors/elementorEditor.js
@@ -4,34 +4,30 @@ import { registerElementorDataHookAfter } from '../elementorHook';
  * Creates an elementor notice.
  *
  * @param {string} content The message content.
- * @param {object} options The notice options.
+ * @param {Object} options The notice options.
  *
  * @return {void}
  */
 export function createNotice( content, options ) {
+	let actions = null;
 
-    let actions = null;
+	if ( options.actions ) {
+		actions = [];
 
-    if (options.actions) {
-
-        actions = [];
-
-        options.actions.forEach(button => {
-
-            actions.push({
-                name: 'wordproof_notice_button',
-                text: button.label,
-                callback() {
-                    button.onClick()
-                },
-            });
-            
-        })
-    }
+		options.actions.forEach( ( button ) => {
+			actions.push( {
+				name: 'wordproof_notice_button',
+				text: button.label,
+				callback() {
+					button.onClick();
+				},
+			} );
+		} );
+	}
 
 	window.elementor.notifications.showToast( {
 		message: content,
-        buttons: actions
+		buttons: actions,
 	} );
 }
 
