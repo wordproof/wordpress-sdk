4c5e96bfae89650e17eb7ef53ef20c8cb189b999
Marijn Bent <marijn@wordproof.com>
2022-03-14 01:13:15 +0700

    add comments

--- resources/js/helpers/data.js
+++ resources/js/helpers/data.js
@@ -1,5 +1,11 @@
 // eslint-disable-next-line no-undef
 const { get } = lodash;
 
+/**
+ * Helper to get data from added JavaScript window variable.
+ *
+ * @param prop
+ * @returns {*}
+ */
 export const getData = ( prop ) =>
 	get( window, `wordproofSdk.data${ prop ? `.${ prop }` : '' }`, {} );
--- resources/js/helpers/wait.js
+++ resources/js/helpers/wait.js
@@ -1,3 +1,9 @@
+/**
+ * Pause execution for a given amount of milliseconds.
+ *
+ * @param {int} time
+ * @returns {Promise<unknown>}
+ */
 export function wait(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
