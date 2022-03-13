23a477158b01ae6e52c4a72550918f434f3e3eaa
Marijn Bent <marijn@wordproof.com>
2022-03-14 00:59:20 +0700

    get unprotected posts for postype

--- app/Helpers/PostTypeHelper.php
+++ app/Helpers/PostTypeHelper.php
@@ -13,4 +13,24 @@ class PostTypeHelper
     {
         return array_values(get_post_types(['public' => true]));
     }
+    
+    public static function getUnprotectedPosts( $postType ) {
+        $query = [
+            'post_type'      => [ $postType ],
+            'fields'         => 'ids',
+            'posts_per_page' => - 1,
+            'post_status' => [ 'publish', 'inherit' ],
+            'meta_query'  => [
+                [
+                    'key'     => '_wordproof_blockchain_transaction',
+                    'compare' => 'NOT EXISTS'
+                ]
+            ],
+        ];
+        
+        $query = new \WP_Query( $query );
+        
+        return ['count' => $query->found_posts, 'postIds' => $query->posts];
+        
+    }
 }
