1f194b4a375b63112f4b4f868496ecda9696fa1b
Marijn Bent <marijn@wordproof.com>
2022-03-20 15:33:55 +0700

    add helpers to open authentication and settings

--- resources/js/helpers/event.js
+++ resources/js/helpers/event.js
@@ -7,3 +7,17 @@ export function dispatch( name ) {
 	const event = new window.CustomEvent( name );
 	window.dispatchEvent( event );
 }
+
+/**
+ * Helper to open the WordProof authentication.
+ */
+export function openAuthentication() {
+    dispatch('wordproof:open_authentication')
+}
+
+/**
+ * Helper to open the WordProof settings.
+ */
+export function openSettings() {
+    dispatch('wordproof:open_settings')
+}
